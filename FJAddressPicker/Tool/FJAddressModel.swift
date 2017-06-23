c33169f739de327679b37b552cf1b443eca63a09
jun <2546863989@qq.com>
2017-06-23 16:24:15 +0800

    JUN 更改文件结构

---
+++ FJAddressPicker/Tool/FJAddressModel.swift
@@ -0,0 +1,17 @@
+//
+//  FJAddressItem.swift
+//  FJAddressPickerDemo
+//
+//  Created by jun on 2017/6/20.
+//  Copyright © 2017年 JUN. All rights reserved.
+//
+
+import UIKit
+
+class FJAddressModel: NSObject {
+    var parent_id: Int = 0
+    var id: Int = 0
+    var level: Int = 0
+    var name: String!
+
+}
